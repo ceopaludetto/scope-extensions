2d6bc953110e218804ce7b2e53620ca9298c2671
Carlos Eduardo <ceo.paludetto@gmail.com>
2024-03-01 18:04:34 -0300

    fix: modern module resolution issues

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	entry: ["src/index.ts"],
 	format: ["cjs", "esm"],
 	clean: true,
-	dts: true,
+	dts: { banner: "export {};\n" }, // The banner should be keeped to typescript consider the file as a module
 	sourcemap: "inline",
 	treeshake: true,
 });
